7e801a0fcd93d79c02104311441f115f0fc90f63
Rohan Upponi <rponi70@gmail.com>
2019-02-04 22:55:41 -0700

    There there...

--- js/custom.js
+++ js/custom.js
@@ -58,4 +58,45 @@ $(document).ready(function() {
        rotated = !rotated;
        translated = !translated;
    });
+
+   $(".link-ref").click(function() {
+       var targetAngle = document.getElementById("nav-icon"), deg = rotated ? 0 : -180;
+
+       targetAngle.style.webkitTransform = "rotate("+deg+"deg)";
+       targetAngle.style.mozTransform = "rotate("+deg+"deg)";
+       targetAngle.style.oTransform = "rotate("+deg+"deg)";
+       targetAngle.style.msTransform = "rotate("+deg+"deg)";
+       targetAngle.style.transform = "rotate("+deg+"deg)";
+
+       var aboutPos = document.getElementById("about-link"), aboutOffset = translated ? 0 : -3;
+
+       aboutPos.style.webkitTransform = "translate(0px, "+aboutOffset+"rem)";
+       aboutPos.style.mozTransform = "translate(0px, "+aboutOffset+"rem)";
+       aboutPos.style.oTransform = "translate(0px, "+aboutOffset+"rem)";
+       aboutPos.style.msTransform = "translate(0px, "+aboutOffset+"rem)";
+       aboutPos.style.transform = "translate(0px, "+aboutOffset+"rem)";
+
+       var resumePos = document.getElementById("resume-link"), resumeOffset = translated ? 0 : -6;
+
+       resumePos.style.webkitTransform = "translate(0px, "+resumeOffset+"rem)";
+       resumePos.style.mozTransform = "translate(0px, "+resumeOffset+"rem)";
+       resumePos.style.oTransform = "translate(0px, "+resumeOffset+"rem)";
+       resumePos.style.msTransform = "translate(0px, "+resumeOffset+"rem)";
+       resumePos.style.transform = "translate(0px, "+resumeOffset+"rem)";
+
+       var portfolioPos = document.getElementById("portfolio-link"), portfolioOffset = translated ? 0 : -9;
+
+       portfolioPos.style.webkitTransform = "translate(0px, "+portfolioOffset+"rem)";
+       portfolioPos.style.mozTransform = "translate(0px, "+portfolioOffset+"rem)";
+       portfolioPos.style.oTransform = "translate(0px, "+portfolioOffset+"rem)";
+       portfolioPos.style.msTransform = "translate(0px, "+portfolioOffset+"rem)";
+       portfolioPos.style.transform = "translate(0px, "+portfolioOffset+"rem)";
+
+       var listBackground = document.getElementById("list-background"), length = translated ? 0 : -25;
+
+       listBackground.style.mozTransform = "scale(1, "+length+")";
+       listBackground.style.oTransform = "scale(1, "+length+")";
+       listBackground.style.msTransform = "scale(1, "+length+")";
+       listBackground.style.transform = "scale(1, "+length+")";
+   });
 });
