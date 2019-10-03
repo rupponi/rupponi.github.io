9d17a27601267cd8494545f10798c45e0f5c43be
rupponi <upponirohan@gmail.com>
2019-10-02 20:50:21 -0700

    Re-factoring.

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
    $('a[href^="#"]').on('click',function (event) {
       event.preventDefault();
 
-      var tarDiv = this.hash;
-      var $tarDiv = $(tarDiv);
+      let tarDiv = this.hash;
+      let $tarDiv = $(tarDiv);
 
       $('html, body').stop().animate({
          'scrollTop': $tarDiv.offset().top
@@ -16,7 +16,7 @@ $(document).ready(function() {
    var translated = false;
 
    $("#nav-icon").click(function() {
-       var targetAngle = document.getElementById("nav-icon"), deg = rotated ? 0 : 180;
+       let targetAngle = document.getElementById("nav-icon"), deg = rotated ? 0 : 180;
 
        targetAngle.style.webkitTransform = "rotate("+deg+"deg)";
        targetAngle.style.mozTransform = "rotate("+deg+"deg)";
@@ -24,7 +24,7 @@ $(document).ready(function() {
        targetAngle.style.msTransform = "rotate("+deg+"deg)";
        targetAngle.style.transform = "rotate("+deg+"deg)";
 
-       var aboutPos = document.getElementById("about-link"), aboutOffset = translated ? 0 : 3;
+       let aboutPos = document.getElementById("about-link"), aboutOffset = translated ? 0 : 3;
 
        aboutPos.style.webkitTransform = "translate(0px, "+aboutOffset+"rem)";
        aboutPos.style.mozTransform = "translate(0px, "+aboutOffset+"rem)";
@@ -32,7 +32,7 @@ $(document).ready(function() {
        aboutPos.style.msTransform = "translate(0px, "+aboutOffset+"rem)";
        aboutPos.style.transform = "translate(0px, "+aboutOffset+"rem)";
 
-       var resumePos = document.getElementById("resume-link"), resumeOffset = translated ? 0 : 6;
+       let resumePos = document.getElementById("resume-link"), resumeOffset = translated ? 0 : 6;
 
        resumePos.style.webkitTransform = "translate(0px, "+resumeOffset+"rem)";
        resumePos.style.mozTransform = "translate(0px, "+resumeOffset+"rem)";
@@ -40,7 +40,7 @@ $(document).ready(function() {
        resumePos.style.msTransform = "translate(0px, "+resumeOffset+"rem)";
        resumePos.style.transform = "translate(0px, "+resumeOffset+"rem)";
 
-       var portfolioPos = document.getElementById("portfolio-link"), portfolioOffset = translated ? 0 : 9;
+       let portfolioPos = document.getElementById("portfolio-link"), portfolioOffset = translated ? 0 : 9;
 
        portfolioPos.style.webkitTransform = "translate(0px, "+portfolioOffset+"rem)";
        portfolioPos.style.mozTransform = "translate(0px, "+portfolioOffset+"rem)";
@@ -48,7 +48,7 @@ $(document).ready(function() {
        portfolioPos.style.msTransform = "translate(0px, "+portfolioOffset+"rem)";
        portfolioPos.style.transform = "translate(0px, "+portfolioOffset+"rem)";
 
-       var listBackground = document.getElementById("list-background"), length = translated ? 0 : 18;
+       let listBackground = document.getElementById("list-background"), length = translated ? 0 : 18;
 
        listBackground.style.mozTransform = "scale(1, "+length+")";
        listBackground.style.oTransform = "scale(1, "+length+")";
@@ -60,7 +60,7 @@ $(document).ready(function() {
    });
 
    $(".link-ref").click(function() {
-       var targetAngle = document.getElementById("nav-icon"), deg = rotated ? 0 : -180;
+       let targetAngle = document.getElementById("nav-icon"), deg = rotated ? 0 : -180;
 
        targetAngle.style.webkitTransform = "rotate("+deg+"deg)";
        targetAngle.style.mozTransform = "rotate("+deg+"deg)";
@@ -68,7 +68,7 @@ $(document).ready(function() {
        targetAngle.style.msTransform = "rotate("+deg+"deg)";
        targetAngle.style.transform = "rotate("+deg+"deg)";
 
-       var aboutPos = document.getElementById("about-link"), aboutOffset = translated ? 0 : -3;
+       let aboutPos = document.getElementById("about-link"), aboutOffset = translated ? 0 : -3;
 
        aboutPos.style.webkitTransform = "translate(0px, "+aboutOffset+"rem)";
        aboutPos.style.mozTransform = "translate(0px, "+aboutOffset+"rem)";
@@ -76,7 +76,7 @@ $(document).ready(function() {
        aboutPos.style.msTransform = "translate(0px, "+aboutOffset+"rem)";
        aboutPos.style.transform = "translate(0px, "+aboutOffset+"rem)";
 
-       var resumePos = document.getElementById("resume-link"), resumeOffset = translated ? 0 : -6;
+       let resumePos = document.getElementById("resume-link"), resumeOffset = translated ? 0 : -6;
 
        resumePos.style.webkitTransform = "translate(0px, "+resumeOffset+"rem)";
        resumePos.style.mozTransform = "translate(0px, "+resumeOffset+"rem)";
@@ -84,7 +84,7 @@ $(document).ready(function() {
        resumePos.style.msTransform = "translate(0px, "+resumeOffset+"rem)";
        resumePos.style.transform = "translate(0px, "+resumeOffset+"rem)";
 
-       var listBackground = document.getElementById("list-background"), length = translated ? 0 : -18;
+       let listBackground = document.getElementById("list-background"), length = translated ? 0 : -18;
 
        listBackground.style.mozTransform = "scale(1, "+length+")";
        listBackground.style.oTransform = "scale(1, "+length+")";
